da430653ecef79d0fac8b50a5cd1529524427edb
CactusPie <mateuszdudek990@gmail.com>
2021-09-15 11:10:07 +0200

    First release

---
+++ src/Rimworld.CactusPie.NoSlaveRomance/DisableSlaveRomancePatch.cs
@@ -0,0 +1,21 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace Rimworld.CactusPie.NoSlaveRomance
+{
+    [HarmonyPatch(typeof(InteractionWorker_RomanceAttempt), nameof(InteractionWorker_RomanceAttempt.RandomSelectionWeight))]
+    internal class DisableSlaveRomancePatch
+    {
+        private static bool Prefix(ref float __result, Pawn initiator, Pawn recipient)
+        {
+            if (initiator.IsSlave || recipient.IsSlave)
+            {
+                __result = 0f;
+                return false;
+            }
+            
+            return true; 
+        }
+    }
+}
---
+++ src/Rimworld.CactusPie.NoSlaveRomance/NoSlaveRomanceEntryPoint.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using Verse;
+
+namespace Rimworld.CactusPie.NoSlaveRomance
+{
+    [StaticConstructorOnStartup]
+    public static class NoSlaveRomanceEntryPoint
+    {
+        static NoSlaveRomanceEntryPoint()
+        {
+            var harmony = new Harmony("Rimworld.CactusPie.NoSlaveRomance");
+            
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
